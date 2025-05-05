e82e039669a88514ca4ca75d705cd5fc88986f98
Lysov Dmitrii <dmlysov@mail.ru>
2025-05-05 21:28:35 +0300

    Добавил ручку для проверки наличия новых заказов

--- api/serializers/order.py
+++ api/serializers/order.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
 from api.models import Order
+from api.serializers.item_order import ItemOrderAllDataSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+
+class OrderGetNewOrderSerializer(serializers.ModelSerializer):
+    user_tg_id = serializers.CharField(source='user.tg_id')
+    items = ItemOrderAllDataSerializer(many=True, source='itemorder_set')
+
+    class Meta:
+        model = Order
+        fields = ['user_tg_id', 'fullname', 'phone_number', 'delivery_address', 'items']
--- api/viewsets/order.py
+++ api/viewsets/order.py
@@ -1,8 +1,10 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.models import Order
-from api.serializers.order import OrderSerializer
+from api.serializers.order import OrderSerializer, OrderGetNewOrderSerializer
 
 @extend_schema(tags=['Order'])
 @extend_schema_view(
@@ -13,3 +15,11 @@ from api.serializers.order import OrderSerializer
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+
+    @action(detail=False, methods=['get'], url_path='new-order', serializer_class=OrderGetNewOrderSerializer)
+    def get_new_order(self, request):
+        new_orders = self.queryset.filter(status='new')
+        serializer = self.serializer_class(new_orders, many=True)
+        answer = serializer.data
+        self.queryset.update(status='processing')
+        return Response(answer)
